4eda2d91270276215233e4d73343510b7bc012f4
Paul George <paul.george@outlook.in>
2018-01-12 18:08:26 +0530

    Site Pages

--- dist/example/demo.js
+++
@@ -1,8 +0,0 @@
- $(document).ready(function () {
-     DonutWidget.draw();
-     $(".small").DonutWidget({
-         max: 200
-         , value: 50
-         , caption: "jquery Implementation"
-     });
- });
